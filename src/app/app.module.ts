0412931632d7a14c965c73e8c1476466f1435e30
Manish <Manishthakur1297@gmail.com>
2019-03-07 00:14:41 +0530

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FatherComponent } from './father/father.component';
+import { FirstChildComponent } from './father/first-child/first-child.component';
+import { SecondChildComponent } from './father/first-child/second-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FatherComponent,
+    FirstChildComponent,
+    SecondChildComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/father/first-child/second-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '\secondChild',
+  templateUrl: './second-child.component.html',
+  styleUrls: ['./second-child.component.css']
+})
+export class SecondChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
